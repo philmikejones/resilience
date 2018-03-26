fdf1f096c49e33f332f7d7d35b3bf0cc9ec49fd6
Phil Mike Jones <philmikejones@gmail.com>
2018-03-26 23:33:41 +0100

    update colours with var change

--- resilience/app.R
+++ resilience/app.R
@@ -28,6 +28,7 @@ ui <- fluidPage(
     ),
 
     mainPanel(
+      textOutput("var"),
       leafletOutput("map", height = "700px")
     )
 
@@ -38,18 +39,27 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
+  output$var = renderText(input$choose_variable)
   output$map <- renderLeaflet({
 
+    pal = reactive(colorNumeric(
+      palette = "YlGn",
+      domain  = don()[[input$choose_variable]]
+    ))
+
+    fillPal = reactive(~pal()(don[[input$choose_variable]]))
+
     leaflet(don) %>%
-      addPolygons(
-        weight = 1,
-        fillOpacity = 0.6,
-        color = input$choose_variable
-      ) %>%
       addProviderTiles(
         "OpenStreetMap.Mapnik",
         options = providerTileOptions(opacity = 0.33)
+      ) %>%
+      addPolygons(
+        weight = 1,
+        fillOpacity = 0.6,
+        fillColor = fillPal
       )
+
   })
 
 }
