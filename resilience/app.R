564008e452ecd1e03c33dea9ef5a565eb7968fdc
Phil Mike Jones <philmikejones@gmail.com>
2018-04-23 21:36:46 +0100

    update tiles

--- resilience/app.R
+++ resilience/app.R
@@ -9,56 +9,28 @@ vars = readRDS("../data/vars.rds")
 
 ui <- fluidPage(
 
-  titlePanel("Resilient characteristics"),
-
-  sidebarLayout(
-
-    sidebarPanel(
-      selectInput(
-        inputId  = "chosen_var",
-        label    = "Choose variable:",
-        choices  = vars,
-        selected = vars[1]
-      ),
-      width = 3
-    ),
-
     mainPanel(
-      DT::dataTableOutput(outputId = "table"),
-      leafletOutput(outputId = "map", height = "700px")
+      leafletOutput(outputId = "map", height = "700px"),
+      width = 12
     )
-
-  )
 
 )
 
 
 server <- function(input, output) {
 
-  don_subset <-
-    reactive({
-      don %>%
-        select(code, name, input$chosen_var)
-    })
-
-  output$table <- DT::renderDataTable({
-    don_subset() %>% sf::st_set_geometry(NULL)
-  })
-
-  colourpal <- reactive({
-    colorBin("Blues", domain = don[[input$chosen_var]])
-  })
-
   output$map <- renderLeaflet({
 
-    leaflet() %>%
-      addProviderTiles("OpenStreetMap.Mapnik")
+    pal = colorNumeric("YlOrRd", don$`Neighbourhood cohesion`)
 
-  })
+    leaflet(don) %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      addPolygons(
+        weight = 1,
+        fillColor = ~pal(don$`Neighbourhood cohesion`),
+        fillOpacity = 0.5
+      )
 
-  observe({
-    leafletProxy("map", data = don_subset()) %>%
-      addPolygons(weight = 1, fillColor = colourpal, fillOpacity = 0.4)
   })
 
 }
