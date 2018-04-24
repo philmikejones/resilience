afce0e74110078fe616863d1df5f112e2e37c4f4
Phil Mike Jones <philmikejones@gmail.com>
2018-04-24 12:47:37 +0100

    use fixed legend breaks

--- resilience/app.R
+++ resilience/app.R
@@ -10,8 +10,17 @@ vars = readRDS("../data/vars.rds")
 ui <- fluidPage(
 
   mainPanel(
+
+    selectInput(
+      inputId  = "selected_var",
+      label    = "Choose variable to plot",
+      selected = vars[2],
+      choices  = vars
+    ),
+
     leafletOutput(outputId = "map", height = "700px"),
     width = 12
+
   )
 
 )
@@ -21,19 +30,20 @@ server <- function(input, output) {
 
   output$map <- renderLeaflet({
 
-    pal = colorNumeric("YlOrRd", don$`Neighbourhood cohesion`)
+    var = reactive(don %>% select(code, name, input$selected_var))
+    pal = colorNumeric("YlOrRd", domain = c(0, 16555))
 
     leaflet(don) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       addPolygons(
         weight = 1,
-        fillColor = ~pal(don$`Neighbourhood cohesion`),
+        fillColor = ~ pal(var()[[input$selected_var]]),
         fillOpacity = 0.6
       ) %>%
       addLegend(
         "bottomright",
         pal = pal,
-        values = ~ don$`Neighbourhood cohesion`,
+        values = ~ c(0, 16555),
         opacity = 0.6,
         title = "Count (persons)"
       )
